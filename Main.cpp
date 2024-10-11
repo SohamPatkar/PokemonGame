fff5f91f912702c922ba6cbf47920c5a781117e8
SohamPatkar <spatkar398@gmail.com>
2024-10-11 16:39:27 +0530

    Added Main function with _playerName variable

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,9 @@ string _playerName;
 
 int main()
 {
+    cout<<"Trainer! Before you embark on your journey, the Pokémon world needs to know who you are!"<<endl; 
+    cout<<"Time to step up and tell us your name. And remember, a great name is the start of every great adventure!"<<endl;
     cout<<"Enter your name"<<endl;
     cin >> _playerName;
-    cout<<"Great Start "<<_playerName<<" looks like you have understood the main() function properly now!"<<endl;
+    cout<<"Great Start "<<_playerName<<" Looks like you’ve mastered the opening of every code adventure. Keep this up, and you’ll be a coding master in no time!"<<endl;
 }
