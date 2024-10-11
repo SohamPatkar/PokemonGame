f7f82870683601926ef10631d15ec7649b2a9d95
SohamPatkar <spatkar398@gmail.com>
2024-10-11 16:30:47 +0530

    Main function executed!

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 
+string _playerName;
+
 int main()
 {
-    cout<<"Hello World";
+    cout<<"Enter your name"<<endl;
+    cin >> _playerName;
+    cout<<"Great Start "<<_playerName<<" looks like you have understood the main() function properly now!"<<endl;
 }
