8f450423d032f618459ad0d1454a6a60fbef6ea6
SohamPatkar <spatkar398@gmail.com>
2024-10-11 16:53:46 +0530

    Added Main function with pokemon choice variable

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-string _playerName;
+string _playerName, _playerChoice;
 
 int main()
 {
@@ -10,4 +10,24 @@ int main()
     cout<<"Enter your name"<<endl;
     cin >> _playerName;
     cout<<"Great Start "<<_playerName<<" Looks like you’ve mastered the opening of every code adventure. Keep this up, and you’ll be a coding master in no time!"<<endl;
+    cout<<"Please choose a pokemon to Start"<<endl;
+    cout<<"Type B for Bulbasaur, C for Charmander and S for Squirtle"<<endl;
+    cin >> _playerChoice;
+
+    if(_playerChoice == "b" || _playerChoice == "B")
+    {
+        cout<<"You chose Bulbasaur! A wise choice."<<endl;
+    }
+    else if(_playerChoice == "c" || _playerChoice == "C")
+    {
+        cout<<"You chose Charmander! A fiery choice."<<endl;
+    }
+    else if(_playerChoice == "s" || _playerChoice == "S")
+    {
+        cout<<"You chose Squirtle! A cool choice."<<endl;
+    }
+    else
+    {
+        cout<<"Invalid choice. Please restart the game."<<endl;
+    }
 }
